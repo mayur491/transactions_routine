9f97af4e28616b73285b01c255364a16b082795b
Mayur Somani <mayur.somani@paytm.com>
2024-10-09 14:32:27 +0530

    added Swagger
    added Swagger

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Stop the script if any command fails
+set -e
+
+# Default environment to 'local' if not provided
+ENV=${1:-local}
+
+echo "Building and running application for environment: $ENV"
+
+# Print commands and their arguments as they are executed
+set -x
+
+# Clean and package the project
+mvn clean compile package -DskipTests -P$ENV -Dspring.profiles.active=$ENV
+
+# Run the Spring Boot application
+java -jar rest/target/*.jar
